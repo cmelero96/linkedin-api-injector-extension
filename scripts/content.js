b711ef7e18cc43321d3689e04f66848327a0c7b8
carlos.melero <cmelero96@gmail.com>
2022-01-05 17:13:31 +0100

    Insert data as raw text just below the main panel

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,10 @@
 const manageApiData = ({ data }) => {
-  console.log(data);
+  const mainPanel = document.getElementById('ember34');
+
+  mainPanel.parentNode.insertBefore(
+    document.createTextNode(JSON.stringify(data)),
+    mainPanel.nextSibling
+  );
 };
 
 chrome.runtime.onMessage.addListener(manageApiData);
