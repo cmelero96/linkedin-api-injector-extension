c09c818f7bed6fa9710ed4b8691e2ab5ee6078f2
carlos.melero <cmelero96@gmail.com>
2022-01-05 18:00:13 +0100

    Create a div for every element searched

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,52 @@
 const manageApiData = ({ data }) => {
-  const mainPanel = document.getElementById('ember34');
+  const extensionId = 'api-injected-data';
+  let apiInjectorPanel = document.getElementById(extensionId);
 
-  mainPanel.parentNode.insertBefore(
-    document.createTextNode(JSON.stringify(data)),
-    mainPanel.nextSibling
-  );
+  if (apiInjectorPanel) {
+    console.log('Emptying old panel...');
+    // If the panel already existed, delete all of its contents
+    while (apiInjectorPanel.firstChild) {
+      apiInjectorPanel.firstChild.remove();
+    }
+  } else {
+    console.log('Creating new panel...');
+    // If the panel didn't exist, create it from scratch
+    const mainUserPanel = document.getElementById('ember34');
+    const newPanel = document.createElement('div');
+    newPanel.id = extensionId;
+    mainUserPanel.parentNode.insertBefore(newPanel, mainUserPanel.nextSibling);
 
-  navigate(data, '__userData__', mainPanel);
+    apiInjectorPanel = newPanel;
+  }
+
+  navigate(data, '__userData__', apiInjectorPanel);
+  console.log('Injection finished.');
 };
 
-const navigate = (value, key, parent) => {
+const navigate = (value, key, parentElement) => {
+  const newDiv = createDiv(key, parentElement);
+
   if (typeof value !== 'object') {
     // Primitive type: String or number most likely
-    console.log(key + ': ' + value);
+    newDiv.innerHTML = key.split('-').pop() + ': ' + value;
   } else if (Array.isArray(value)) {
     // Array of elements
-    value.forEach((element, i) => navigate(element, `${key}-${i}`));
+    value.forEach((element, i) => navigate(element, `${key}-${i}`, newDiv));
   } else if (value !== null) {
     // Object
     for (property in value) {
-      navigate(value[property], `${key}-${property}`);
+      navigate(value[property], `${key}-${property}`, newDiv);
     }
   }
 };
+
+const createDiv = (id, parent) => {
+  const element = document.createElement('div');
+  element.id = id;
+  element.innerHTML = id.split('-').pop();
+  parent.appendChild(element);
+
+  return element;
+};
+
 chrome.runtime.onMessage.addListener(manageApiData);
