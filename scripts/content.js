6b4bc78a6e25be3e86586c4fb4726536f5455f4f
carlos.melero <cmelero96@gmail.com>
2022-01-05 17:41:32 +0100

    Created method to navigate object recursively

--- scripts/content.js
+++ scripts/content.js
@@ -5,6 +5,22 @@ const manageApiData = ({ data }) => {
     document.createTextNode(JSON.stringify(data)),
     mainPanel.nextSibling
   );
+
+  navigate(data, '__userData__', mainPanel);
 };
 
+const navigate = (value, key, parent) => {
+  if (typeof value !== 'object') {
+    // Primitive type: String or number most likely
+    console.log(key + ': ' + value);
+  } else if (Array.isArray(value)) {
+    // Array of elements
+    value.forEach((element, i) => navigate(element, `${key}-${i}`));
+  } else if (value !== null) {
+    // Object
+    for (property in value) {
+      navigate(value[property], `${key}-${property}`);
+    }
+  }
+};
 chrome.runtime.onMessage.addListener(manageApiData);
